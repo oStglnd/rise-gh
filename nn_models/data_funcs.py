26ba095871808bc0196df3064ebfbf61db3046a6
oStglnd <stigland@kth.se>
2023-10-30 19:49:20 +0100

    Added initial k-fold loop w. validation data

--- nn_models/data_funcs.py
+++ nn_models/data_funcs.py
@@ -50,15 +50,15 @@ def seq_maker(data, targets, temps, dates, t_steps, n_steps):
     mask = [idx not in flags for idx in range(len(sequences))]
     
     sequences = sequences[mask]
-    targets = targets[n_steps:][mask].values
-    temps_t = temps[n_steps-t_steps:-t_steps][mask].values
-    temps = temps[:-n_steps][mask].values # TEMPORARY
+    targets = targets[n_steps:][mask].values[:, np.newaxis]
+    temps_t = temps[n_steps-t_steps:-t_steps][mask].values[:, np.newaxis]
+    temps = temps[:-n_steps][mask].values[:, np.newaxis]
     dates = dates[:-n_steps][mask].values
     
     return sequences, targets, temps, temps_t, dates
 
 # define func f. data normalization
-def data_norm(data_train, data_test):
+def data_norm(data_train, data_test, data_val):
     col_params = {}
     for col in data_train.columns:
 
@@ -71,6 +71,7 @@ def data_norm(data_train, data_test):
 
         data_train[col] = (data_train[col] - mean) / std
         data_test[col] = (data_test[col] - mean) / std
+        data_val[col] = (data_val[col] - mean) / std
 
         col_params[col] = {
             'mean':mean,
@@ -79,7 +80,7 @@ def data_norm(data_train, data_test):
             'min':min_val
             }
     
-    return data_train, data_test, col_params
+    return data_train, data_test, data_val, col_params
 
 def k_fold_data(data, k_idx, k_frac, m, cols, t_steps, n_steps, setpoint, shuffle):
     
@@ -87,8 +88,9 @@ def k_fold_data(data, k_idx, k_frac, m, cols, t_steps, n_steps, setpoint, shuffl
     days = data.groupby(['month', 'day'], sort=False).count().index.values
     
     # get days for K:th fold
-    train_n = int(len(days) * (1 - k_frac))
-    test_n = len(days) - train_n
+    # train_n = int(len(days) * (1 - k_frac))
+    # test_n = len(days) - train_n
+    test_n = int(len(days) * k_frac)
     
     # split days by test and train
     days_test = days[int(k_idx*test_n):int((k_idx+1)*test_n)].tolist()
@@ -96,33 +98,47 @@ def k_fold_data(data, k_idx, k_frac, m, cols, t_steps, n_steps, setpoint, shuffl
     data_train = data.loc[~mask_test].copy()
     data_test = data.loc[mask_test].copy()
 
+    # get validation data
+    days_train = np.unique(data_train.index.droplevel(-1).droplevel(-1).droplevel(-1).values)
+    val_n = int(len(days_train) * k_frac)
+    days_val = np.random.choice(days_train, val_n, replace=False).tolist()
+    mask_val = np.array([day in days_val for day in data_train.index.droplevel(-1).droplevel(-1).droplevel(-1).values])
+    data_val = data_train.loc[mask_val].copy()
+    data_train = data_train.loc[~mask_val].copy()
+    
     # reduce to m-min observations
     data_train, dates_train = data_reduce(data_train, m)
     data_test, dates_test = data_reduce(data_test, m)
+    data_val, dates_val = data_reduce(data_val, m)
 
     # remove setpoint
     if setpoint:
-        data_train[('temperatures', 'TA01_GT10X_GM10X')] -= data_train.setpoints.TA01_GT10X_GM10X + 20
-        data_test[('temperatures', 'TA01_GT10X_GM10X')] -= data_test.setpoints.TA01_GT10X_GM10X + 20
+        data_train[('temperatures', 'TA01_GT10X_GM10X')] += 20 - data_train.setpoints.TA01_GT10X_GM10X
+        data_test[('temperatures', 'TA01_GT10X_GM10X')] += 20 - data_test.setpoints.TA01_GT10X_GM10X
+        data_val[('temperatures', 'TA01_GT10X_GM10X')] += 20 - data_val.setpoints.TA01_GT10X_GM10X
     
     # filter data
     data_train = data_train[cols].copy()
     data_test = data_test[cols].copy()
+    data_val = data_val[cols].copy()
     
     # normalize
-    data_train, data_test, col_params = data_norm(data_train, data_test)
+    data_train, data_test, data_val, col_params = data_norm(data_train, data_test, data_val)
     
     # get targets
     targets_train = data_train.pop(('temperatures', 'TA01_GT10X_GM10X'))
     targets_test = data_test.pop(('temperatures', 'TA01_GT10X_GM10X'))
+    targets_val = data_val.pop(('temperatures', 'TA01_GT10X_GM10X'))
     
     # get temp info
     temps_train = targets_train.copy()
     temps_test = targets_test.copy()
+    temps_val = targets_val.copy()
     
     # create sequences    
     sequences_train, targets_train, temps_train, temps_t_train, dates_train = seq_maker(data_train, targets_train, temps_train, dates_train, t_steps, n_steps)
     sequences_test, targets_test, temps_test, temps_t_test, dates_test = seq_maker(data_test, targets_test, temps_test, dates_test, t_steps, n_steps)
+    sequences_val, targets_val, temps_val, temps_t_val, dates_val = seq_maker(data_val, targets_val, temps_val, dates_val, t_steps, n_steps)
     
     # create MASKED sequences
     sequences_masked = sequences_test.copy()
@@ -131,7 +147,6 @@ def k_fold_data(data, k_idx, k_frac, m, cols, t_steps, n_steps, setpoint, shuffl
 
     # shuffle training data randomly
     if shuffle:
-        print(len(targets_train))
         idxs = np.arange(len(targets_train))
         np.random.shuffle(idxs)
 
@@ -144,4 +159,6 @@ def k_fold_data(data, k_idx, k_frac, m, cols, t_steps, n_steps, setpoint, shuffl
     # return tups w. train and test
     train_tup = (sequences_train, targets_train, temps_train, temps_t_train, dates_train)
     test_tup = (sequences_test, targets_test, temps_test, temps_t_test, sequences_masked, dates_test)
-    return train_tup, test_tup, col_params
+    val_tup = (sequences_val, targets_val, temps_val, temps_t_val, dates_val)
+    
+    return train_tup, test_tup, val_tup, col_params
